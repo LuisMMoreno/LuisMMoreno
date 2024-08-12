edeadcb6a82cd21ae7716efd309bbef87403cbb7
LuisMMoreno <90772221+LuisMMoreno@users.noreply.github.com>
2024-08-12 07:51:32 -0500

    Update JavaApplication1.java

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -24,18 +24,11 @@ public class JavaApplication1 {
             System.out.print("Digite el numero de tabla que desea conocer: ");
             int num = a.nextInt();
 
-            System.out.println(num +"x"+1+"="+(num*1));
-            System.out.println(num +"x"+2+"="+(num*2));
-            System.out.println(num +"x"+3+"="+(num*3));
-            System.out.println(num +"x"+4+"="+(num*4));
-            System.out.println(num +"x"+5+"="+(num*5));
-            System.out.println(num +"x"+6+"="+(num*6));
-            System.out.println(num +"x"+7+"="+(num*7));
-            System.out.println(num +"x"+8+"="+(num*8));
-            System.out.println(num +"x"+9+"="+(num*9));
-            System.out.println(num +"x"+10+"="+(num*10));
-            System.out.println(num +"x"+11+"="+(num*11));
-            System.out.println(num +"x"+12+"="+(num*12));
+            for (int i=1;i=12;i++){
+
+                System.out.println(num +"x"+i+"="+(num*i));
+            
+            }
             
             System.out.print("Desea consultar otra tabla, precione 1 para si, 2 para no: ");
             xd=a.nextInt();
